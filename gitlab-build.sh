225108356a1e37a246fd8358d421e531014e1456
Bruno Queiros <bqueiros@gmail.com>
2019-08-17 10:03:41 +0000

    Update gitlab-build.sh

--- gitlab-build.sh
+++ gitlab-build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sudo apt-get update; sudo apt-get install linux-modules-$(uname -r) -y
+sudo apt-get update; sudo apt-get upgrade -y ; sudo apt-get dist upgrade -y
+sudo apt-get update; sudo apt-get install sshpass linux-modules-$(uname -r) -y
 sudo apt-get install sshpass fuse -y ; sudo modprobe fuse 
 
 wget https://raw.githubusercontent.com/Nitrux/nitrux-repository-util/master/build-vvave.sh
@@ -10,7 +11,9 @@ chmod +x build-vvave.sh
 ./build-vvave.sh
 
 
+## Export to the correct folder 
+
 export SSHPASS=$DEPLOY_PASS
 
-sshpass -e scp -q -o stricthostkeychecking=no *.AppImage $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
-#sshpass -e ssh $DEPLOY_USER@$DEPLOY_HOST 'bash /home/packager/repositories/nomad-desktop/repositories_util.sh'
+sshpass -e scp -q -o stricthostkeychecking=no /builds/nitrux/mauikit/vvave/vvave/build/*.AppImage  $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
+sshpass -e ssh $DEPLOY_USER@$DEPLOY_HOST 'bash /home/lnxslck/appimages/appimages_util.sh'
