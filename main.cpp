9c994d8a3b12f404eab7396845be97931f9d9455
Camilo Higuita <chiguitar@unal.edu.co>
2020-03-12 11:06:48 -0500

    do not reffer to csd just yet

--- main.cpp
+++ main.cpp
@@ -11,8 +11,6 @@
 
 #ifdef STATIC_MAUIKIT
 #include "3rdparty/mauikit/src/mauikit.h"
-#else
-#include <MauiKit/mauiapp.h>
 #endif
 
 #ifdef Q_OS_ANDROID
@@ -117,8 +115,6 @@ int main(int argc, char *argv[])
 
 #ifdef STATIC_MAUIKIT
     MauiKit::getInstance().registerTypes();
-#else
-    MauiApp::instance()->setEnableCSD(true);
 #endif
     QtWebView::initialize();
 
