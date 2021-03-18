fbca02f53c12ccf6a4a33cb703beb6846a3216ec
Camilo Higuita <chiguitar@unal.edu.co>
2021-03-17 22:44:59 -0500

    udpate build info label

--- src/main.cpp
+++ src/main.cpp
@@ -84,7 +84,7 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
 
     KLocalizedString::setApplicationDomain("vvave");
     KAboutData about(
-        QStringLiteral("vvave"), i18n("Vvave"), VVAVE_VERSION_STRING, i18n("Vvave lets you organize, browse and listen to your local and online music collection."), KAboutLicense::LGPL_V3, i18n("© 2019-%1 Nitrux Development Team", QString::number(QDate::currentDate().year())),i18n("Built from ") + QString(GIT_BRANCH) + " " + QString(GIT_COMMIT_HASH));
+        QStringLiteral("vvave"), i18n("Vvave"), VVAVE_VERSION_STRING, i18n("Vvave lets you organize, browse and listen to your local and online music collection."), KAboutLicense::LGPL_V3, i18n("© 2019-%1 Nitrux Development Team", QString::number(QDate::currentDate().year())),QString(GIT_BRANCH) + "/" + QString(GIT_COMMIT_HASH));
     about.addAuthor(i18n("Camilo Higuita"), i18n("Developer"), QStringLiteral("milo.h@aol.com"));
     about.setHomepage("https://mauikit.org");
     about.setProductName("maui/vvave");
